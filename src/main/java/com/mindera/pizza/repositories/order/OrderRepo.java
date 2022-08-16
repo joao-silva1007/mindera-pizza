53c03b4e3d16d6ec0c7aabe12cfc5f7e4c07c48b
Joao Silva <joao.silva@avios.com>
2022-08-16 19:27:40 +0100

    #3 Removed unused import

--- src/main/java/com/mindera/pizza/repositories/order/OrderRepo.java
+++ src/main/java/com/mindera/pizza/repositories/order/OrderRepo.java
@@ -3,7 +3,6 @@ package com.mindera.pizza.repositories.order;
 import com.mindera.pizza.domain.order.OrderStatus;
 import com.mindera.pizza.domain.order.RestaurantOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
