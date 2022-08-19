62c1e26e89f18e6b8683e42b4971acafe95e6ccf
Joao Silva <joao.silva@avios.com>
2022-08-19 15:17:25 +0100

    #4 Changed annotations Changed request mappings

--- src/main/java/com/mindera/pizza/controllers/order/RestaurantOrderController.java
+++ src/main/java/com/mindera/pizza/controllers/order/RestaurantOrderController.java
@@ -3,6 +3,7 @@ package com.mindera.pizza.controllers.order;
 import com.mindera.pizza.domain.order.RestaurantOrder;
 import com.mindera.pizza.dto.order.CreateRestaurantOrderDTO;
 import com.mindera.pizza.services.order.RestaurantOrderService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("order")
+@RequestMapping("/order")
+@AllArgsConstructor
 public class RestaurantOrderController {
     private final RestaurantOrderService restaurantOrderService;
 
-    public RestaurantOrderController(RestaurantOrderService restaurantOrderService) {
-        this.restaurantOrderService = restaurantOrderService;
-    }
-
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<RestaurantOrder> createRestaurantOrder(@RequestBody CreateRestaurantOrderDTO restaurantOrderDTO) {
         return new ResponseEntity<>(restaurantOrderService.createOrder(restaurantOrderDTO), HttpStatus.CREATED);
     }
