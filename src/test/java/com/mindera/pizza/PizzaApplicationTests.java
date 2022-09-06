1aec9236e3ecdb7b1d24838fdaee40680ae59ead
Joao Silva <joao.silva@avios.com>
2022-09-06 11:37:51 +0100

    #18 changed sql file to prevent duplicate categories

--- src/test/java/com/mindera/pizza/PizzaApplicationTests.java
+++ src/test/java/com/mindera/pizza/PizzaApplicationTests.java
@@ -1,13 +1,14 @@
 package com.mindera.pizza;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PizzaApplicationTests {
 
-	/*@Test
+	@Test
 	void contextLoads() {
-	}*/
+	}
 
 }
